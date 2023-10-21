21f1a9b943ecbfecdc096920a4a605db114f84ec
Kirill <ekirillo@rambler.ru>
2023-10-22 04:16:42 +0500

    num27

---
+++ num27/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = 1;
+int result = 0;
+int ost = 0;
+while(A > B)
+{
+    ost = A % 10;
+    A = A / 10;
+    result = result + ost;
+}
+System.Console.WriteLine(result);
