d25051a2faeca185f8baeef1a788d6b08338101e
Kirill <ekirillo@rambler.ru>
2023-10-22 04:08:47 +0500

    n25

--- num25/Program.cs
+++ num25/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите степень: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int result = 1;
+while(count < B)
+{
+    result = result * A;
+    count++;
+}
+System.Console.WriteLine(result);
